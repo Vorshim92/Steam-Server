ab1b8c89b142ae514239675fb89d123c13a2db60
Vorshim92 <vorshim92@gmail.com>
2024-06-24 13:03:30 +0200

    search bar

--- steam-server-frontend/src/pages/Products.tsx
+++ steam-server-frontend/src/pages/Products.tsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
 import { Game } from "../interfaces/types";
-import { useAppSelector, useAppDispatch } from "../redux/hooks";
 import ProductCard from "../components/ProductCard";
+import { Link } from "react-router-dom";
 
 const Products = () => {
   const [games, setGames] = useState<Game[]>([]);
-  const dispatch = useAppDispatch();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState<Game[]>([]);
 
   useEffect(() => {
     const fetchGames = async () => {
@@ -25,9 +26,33 @@ const Products = () => {
     fetchGames();
   }, []);
 
+  useEffect(() => {
+    if (searchQuery.length > 0) {
+      const filteredGames = games.filter((game) => game.name && game.name.toLowerCase().includes(searchQuery.toLowerCase()));
+      setSearchResults(filteredGames);
+    } else {
+      setSearchResults([]);
+    }
+  }, [searchQuery, games]);
+
   return (
     <div className="container">
-      <div className="row gap-3">{games && games.map((game) => <ProductCard game={game} key={game.id} />)}</div>
+      <div className="row row-gap-3 mt-5">
+        <div className="col text-center text-white">
+          <h1>SCEGLI IL TUO GIOCO</h1>
+          <input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} placeholder="Cerca" />
+          {searchResults.length > 0 && (
+            <div className="search-results  w-100 bg-white shadow-lg">
+              {searchResults.map((result) => (
+                <Link key={result.id} to={`/games/${result.id}`} className="search-result-item d-block p-2 text-dark" onClick={() => setSearchQuery("")}>
+                  {result.name}
+                </Link>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="row row-gap-3 mt-5">{games && games.map((game) => <ProductCard game={game} key={game.id} />)}</div>
     </div>
   );
 };
